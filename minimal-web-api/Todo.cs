0f9edc46205068f94cb391ad19cc8f137a5c4ab6
czy30121 <czy30121@gmail.com>
2023-12-13 00:17:09 +0900

    initialize the project

---
+++ minimal-web-api/Todo.cs
@@ -0,0 +1,10 @@
+﻿namespace minimal_web_api
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+        public string? Secret { get; set; }
+    }
+}
---
+++ minimal-web-api/TodoDb.cs
@@ -0,0 +1,11 @@
+﻿namespace minimal_web_api
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class TodoDb : DbContext
+    {
+        public TodoDb(DbContextOptions<TodoDb> options) : base(options) { }
+
+        public DbSet<Todo> Todos => Set<Todo>();
+    }
+}
